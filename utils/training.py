4d50c211605e9f0732081c264c9e9065e7e18878
Christian Jarvers <christian.jarvers@gmx.de>
2021-07-30 10:26:35 +0200

    Extended functionality of mulitGPU training function.

--- utils/training.py
+++ utils/training.py
@@ -38,6 +38,7 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
 import torch.multiprocessing as mp
+from utils import AverageMeter()
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
@@ -52,9 +53,39 @@ def setup(rank, world_size):
 # taken from PyTorch tutorial
 def cleanup():
     dist.destroy_process_group()
+    
 
+def trainloop(data, model, loss_fn, opt, dev):
+    avgloss = AverageMeter()
+    model.train()
+    for (x, y)  in data:
+        # train on the batch
+        x = x.to(dev)
+        y = y.to(dev)
+        pred = model(x)
+        loss = loss_fn(pred, y)
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        # log the loss
+        avgloss.update(loss.item())
+    return avgloss.avg
 
-def multidata_train(rank, world_size, make_backbone, datasets, decoders, losses, devices):
+def valloop(data, model, metric, dev):
+    avgmetric = AverageMeter()
+    model.eval()
+    with torch.no_grad():
+        for (x, y) in data:
+            x = x.to(dev)
+            y = y.to(dev)
+            pred = model(x)
+            m = metric(pred, y)
+            avgmetric.update(loss.item())
+    return avgmetric.avg
+        
+
+def multidata_train(rank, world_size, make_backbone, datasets, decoders, losses, metrics,
+        devices, log_fn, epochs=50, debug=False):
     '''
     Trains the common network `backbone` on several datasets simultaneously.
     
@@ -62,9 +93,16 @@ def multidata_train(rank, world_size, make_backbone, datasets, decoders, losses,
         *rank (int): Process number that the current copy of the function is run on.
         *world_size (int): Number of processes on which the function is run in parallel.
         *make_backbone: Constructor for network component that is common across all datasets
-        *datasets: The datasets to train on. Each should be an iterable that yields batches.
+        *datasets: The datasets to train on. Each should be a pair of iterables
+                   (training and validation loaders).
         *decoders: The network components that differ for each dataset.
+        *losses: Loss functions to use for training the network(s).
+        *metrics: Functions to evaluate the network with on validation data.
         *devices: The compute devices on which the networks should be trained.
+        *log_fn: Function that receives the epoch number, average loss and metric,
+                 model and decoder state_dicts, and process rank. Can be used for logging.
+        *epochs (int): Number of epochs to train
+        *debug (bool): If True, prints some debug information
         
     The lists `datasets`, `decoders`, `losses`, and `devices` have to be of the same length.
     Essentially, device i will get a batch from dataset i, put it through the backbone,
@@ -76,33 +114,33 @@ def multidata_train(rank, world_size, make_backbone, datasets, decoders, losses,
         'Lists of datasets, decodes, losses, and devices have to have equal length.'
     # set up distributed processing and DDP model
     setup(rank, world_size)
-    dev = devices[rank]
     model = make_backbone().to(dev)
     ddp_model = DDP(model, device_ids=[rank])
     decoder = decoders[rank].to(dev)
     complete_model = nn.Sequential(ddp_model, decoder)
+    traindata, valdata = datasets[rank]
+    dev = devices[rank]
     
-    # add some debug information
-    print(f'Start on process {rank}: {datetime.datetime.now()}')
-    print(f'Running multidata_train on process {rank}, device {dev}')
+    # print some debug information
+    if debug:
+        print(f'Start on process {rank}: {datetime.datetime.now()}')
+        print(f'Running multidata_train on process {rank}, device {dev}')
     
     loss_fn = losses[rank]
+    eval_fn = metrics[rank]
     optimizer = optim.SGD(complete_model.parameters(), lr=0.001)
     
     # run training loop
-    for (x, y) in datasets[rank]:
-        optimizer.zero_grad()
-        x = x.to(dev)
-        y = y.to(dev)
-        y_hat = complete_model(x)
-        loss = loss_fn(y_hat, y)
-        loss.backward()
-        optimizer.step()
-        print(f'Loss on process {rank}: {loss}')
+    for e in range(epochs):
+        avgloss = trainloop(traindata, complete_model, loss_fn, optimizer, dev)
+        avgm = valloop(valdata, complete_model, eval_fn, dev)
+        log_fn(e, avgloss, avgm, model.state_dict(), decoder.state_dict(), rank)
+        
     
-    print(f'Final network parameters on process {rank}: {list(model.parameters())}')
-    print(f'Final decoder parameters on process {rank}: {list(decoder.parameters())}')
-    print(f'End on process {rank}: {datetime.datetime.now()}')
+    if debug:
+        print(f'Final network parameters on process {rank}: {list(model.parameters())}')
+        print(f'Final decoder parameters on process {rank}: {list(decoder.parameters())}')
+        print(f'End on process {rank}: {datetime.datetime.now()}')
     cleanup()
     
     
@@ -129,11 +167,15 @@ def demo_2losses():
     data2 = zip(xs2, ys2)
     loss1 = torch.nn.MSELoss()
     loss2 = torch.nn.CrossEntropyLoss()
+    metric1 = torch.nn.MSELoss()
+    metric2 = lambda pred, y: 0.0
     dev1 = torch.device('cuda:0')
     dev2 = torch.device('cuda:1')
+    log_fn = lambda e, l, m, _, _, rank: print(f'Rank {rank}, epoch {e}: loss is {l},\t metric is {m}')
     # adjusted from PyTorch tutorial
     mp.spawn(multidata_train,
-             args=(2, ToyModel, [data1, data2], [decoder1, decoder2], [loss1, loss2], [dev1, dev2]),
+             args=(2, ToyModel, [(data1, data1), (data2, data2)], [decoder1, decoder2],
+                   [loss1, loss2], [metric1, metric2], [dev1, dev2], 1, True),
              nprocs=2,
              join=True)
 
