c4fed9135e037d2242a0cf947ae47a5e73b4987e
Christian Jarvers <christian.jarvers@gmx.de>
2021-08-02 21:26:48 +0200

    Made video subsampling robust against too short videos.

--- data/utils.py
+++ data/utils.py
@@ -1,10 +1,13 @@
 from math import floor
-
+from torch.nn.functional import pad
 
 def subsample(video, nsamples):
     '''
     Subsamples a video to have the specified number of frames (nsamples).
     '''
     frames = video.size()[0]
-    step = floor(frames / nsamples)
-    return(video[0:nsamples*step:step, :, :, :])
+    if frames >= nsamples:
+        step = floor(frames / nsamples)
+        return(video[0:nsamples*step:step, :, :, :])
+    else: # if there are less frames than we want samples, we need to zero-pad the video.
+        return pad(video, (0, 0, 0, 0, 0, 0, 0, nsamples - frames))
