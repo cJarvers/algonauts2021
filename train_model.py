e1c9a0e692b160bf5dfa077fc2d08300b8a5cf7c
Christian Jarvers <christian.jarvers@gmx.de>
2021-08-10 14:53:18 +0200

    Included cityscapes in training script.

--- train_model.py
+++ train_model.py
@@ -21,7 +21,8 @@ from data.moments_loader import MomentsDataset
 from data.objectron_loader import ObjectronDataset
 from data.youtube_faces_loader import YouTubeFacesDataset
 from data.davis_loader import DAVISDataset
-from models.decoders import ClassDecoder, UNet3DDecoder
+from data.cityscapes_loader import CityscapesDataset
+from models.decoders import ClassDecoder, UNet3DDecoder, Deconv2DDecoder
 from models.resnet3d50 import ResNet3D50Backbone
 from utils.training import multidata_train
 from utils.utils import Logger
@@ -30,8 +31,8 @@ from utils.losses import NT_Xent
 # set up command line parsing
 parser = argparse.ArgumentParser(description='Perform training of 3D-ResNet50 model on multiple datasets.')
 parser.add_argument('--bsize', type=int, default=32, help='Batch size')
-parser.add_argument('-d', '--devices', type=str, nargs='*', default=['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'], help='Names of devices to train on.')
-parser.add_argument('-n', '--nprocs', type=int, default=3, help='Number of processes to launch.')
+parser.add_argument('-d', '--devices', type=str, nargs='*', default=['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3', 'cuda:4'], help='Names of devices to train on.')
+parser.add_argument('-n', '--nprocs', type=int, default=5, help='Number of processes to launch.')
 parser.add_argument('-b', '--batches', type=int, default=1000, help='Number of batches to train.')
 parser.add_argument('--logpath', type=str, default='/mnt/logs/', help='Path to save log files to.')
 parser.add_argument('--ckptpath', type=str, default='/mnt/logs/', help='Path to save checkpoints to.')
@@ -66,6 +67,8 @@ if __name__ == '__main__':
         planes=[512, 256, 128, 64, 64], outplanes=[1024, 512, 256, 64, 64],
         upsample=[True, True, True, False, True],
         finallayer=torch.nn.ConvTranspose3d(64, 2, kernel_size=(1, 2, 2), stride=(1, 2, 2)))
+    cityscapes_decoder = Deconv2DDecoder(inplanes=2048, planes=[512, 256, 128, 64, 64],
+        [1024, 512, 256, 128, 64], torch.nn.Conv2d(64, 34, kernel_size=1))
     if args.resume: # to resume previous training, load weights from previous checkpoint
         log = torch.load(logpath + 'rank0.log')
         batchnum = log['loss'][-1][1]
@@ -80,7 +83,9 @@ if __name__ == '__main__':
         youtube_faces_decoder.load_state_dict(weights)
         weights = torch.load(f'decoder_3_b{batchnum}.ckpt')
         davis_decoder.load_state_dict(weights)
-    decoders = [moments_decoder, objectron_decoder, youtube_faces_decoder, davis_decoder]
+        weights = torch.load(f'decoder_4_b{batchnum}.ckpt')
+        cityscapes.load_state_dict(weights)
+    decoders = [moments_decoder, objectron_decoder, youtube_faces_decoder, davis_decoder, cityscapes_decoder]
 
     # load datasets
     transform = Compose([ConvertImageDtype(torch.float32), Resize((224, 224)),
@@ -99,7 +104,10 @@ if __name__ == '__main__':
     label_transform = Compose([Lambda(squeeze), Lambda(tolong), Lambda(truncate), Resize((224, 224))])
     davis = DAVISDataset('/data/DAVIS', 'training', 16, transform, label_transform)
     davis_loader = DataLoader(davis, batch_size=4, shuffle=True, drop_last=True, num_workers=4)
-    datasets = [(moments_loader, []), (objectron_loader, []), (yt_faces_loader, []), (davis_loader, [])]
+    label_transform = Compose([Lambda(squeeze), Lambda(tolong), Resize((224, 224))])
+    cityscapes = CityscapesDataset('/data/cityscapes', 'training', 16, transform, label_transform)
+    cityscapes_loader = DataLoader(cityscapes, batch_size=args.bsize, shuffle=True, num_workers=8)
+    datasets = [(moments_loader, []), (objectron_loader, []), (yt_faces_loader, []), (davis_loader, []), (cityscapes_loader, [])]
 
     # set up remaining training infrastructure
     devices = (args.devices * len(datasets))[:len(datasets)] # if there are more dataset than devices, distribute
@@ -107,8 +115,9 @@ if __name__ == '__main__':
     objectron_loss = torch.nn.CrossEntropyLoss().cuda()
     yt_faces_loss = NT_Xent(0.1).cuda()
     davis_loss = torch.nn.CrossEntropyLoss().cuda()
-    losses = [moments_loss, objectron_loss, yt_faces_loss, davis_loss]
-    metrics = [(), (), (), ()]
+    cityscapes_loss = torch.nn.CrossEntropyLoss().cuda()
+    losses = [moments_loss, objectron_loss, yt_faces_loss, davis_loss, cityscapes_loss]
+    metrics = [(), (), (), (), ()]
     loggers = [Logger(args.logpath, args.ckptpath, logevery=args.ckptinterval)] * len(datasets)
     if args.resume:
         for i, l in enumerate(loggers):
