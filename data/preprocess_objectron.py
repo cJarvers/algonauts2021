e9c7488ca61c449e38bc5e51cc16fecf86830dc3
Christian Jarvers <christian.jarvers@gmx.de>
2021-08-11 08:32:37 +0200

    Added code to preprocess objectron data and modified data loader.

--- data/preprocess_objectron.py
+++ data/preprocess_objectron.py
@@ -4,12 +4,14 @@ Wrapper / loader for Objectron dataset.
 Assumes that data have been downloaded with `download_objectron_redux.py` script.
 '''
 import os
+import datetime
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torchvision
-from utils import subsample
+from torchvision.transforms import ConvertImageDtype, Resize, Compose, Lambda, Normalize
+from data.utils import subsample
 
-def get_paths(root_dir, suffix):
+def get_paths(root_dir):
     '''
     Traverses the directory `root_dir` to get the paths of all objectron videos.
     '''
@@ -19,11 +21,11 @@ def get_paths(root_dir, suffix):
     paths = []
     for c in classes:
         cpaths = os.listdir(os.path.join(root_dir, c))
-        paths.extend([(os.path.join(root_dir, c, p), class2int[c]) for p in cpaths if p[-len(suffix):] == suffix])
+        paths.extend([(os.path.join(root_dir, c, p), class2int[c]) for p in cpaths])
     return paths
 
 
-class ObjectronDataset(Dataset):
+class ObjectronPreprocessor:
     '''
     Wrapper for the Objectron dataset.
     
@@ -31,30 +33,39 @@ class ObjectronDataset(Dataset):
         *root_dir (str): Directory from which to load data. Should contain ...
         *nframes (int): number of frames to subsample every video to
         *transform: PyTorch transform to apply to the videos/frames
-        *suffix (str): file suffix to load data from; .pt or .mov
     '''
 
-    def __init__(self, root_dir, nframes, transform=None, suffix='.pt'):
+    def __init__(self, root_dir, nframes, transform=None):
         self.root_dir = root_dir
         self.nframes = nframes
         self.transform = transform
-        self.videos = get_paths(root_dir, suffix)
-        # TODO: compute or load mean and standard deviation over complete dataset
-        #       to normalize videos
-        #       Alternatively, we can add a batch-norm layer at the front of the network
+        self.videos = get_paths(root_dir)
     
     def __len__(self):
         return(len(self.videos))
     
-    # Getting one video takes about ... on my computer (without any transforms).
-    # If that is not fast enough for us, we should think about precomputing the tensors
-    # and saving them on disk.
     def __getitem__(self, idx):
+        path, label = self.videos[idx]
+        vid, _, _ = torchvision.io.read_video(path)
+        
+    def convert(self, idx):
         path, label = self.videos[idx]
         vid, _, _ = torchvision.io.read_video(path)
         vid = subsample(vid, self.nframes)
         vid = vid.movedim(3, 0)
         if self.transform:
             vid = self.transform(vid)
-        return(vid, label)
+        basename = path.split('.')[0]
+        torch.save(vid, basename + '.pt')
+        
 
+if __name__ == '__main__':
+    transform = Compose([ConvertImageDtype(torch.float32), Resize((224, 224))])
+    p = ObjectronPreprocessor('/data/objectron', 16, transform)
+    
+    print(f'Preprocessing {len(p)} videos. Time: {datetime.datetime.now()}', flush=True)
+    for i in range(len(p)):
+        p.convert(i)
+        if i+1 % 100 == 0:
+            print(f'Processed {i+1} videos. Time: {datetime.datetime.now()}', flush=True)
+    
